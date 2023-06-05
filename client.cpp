6f32d68fcf8991870b0274cd336ce53dae684fe4
Brandon St Pierre <bstpier1@gmail.com>
2023-06-05 15:53:08 -0700

    working commit

--- client.cpp
+++ client.cpp
@@ -14,24 +14,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <chrono>
+
 using namespace std;
+using namespace std::chrono;
 
 const int BUFFSIZE=1500;
 
 int main(int argc, char *argv[])
 {
-    char *serverName;
-    char serverPort[6] = "12345";
-    char *databuf;
+    char *serverName; //name of server
+    char port[6]; //server port
+    int repetitions; //repetition of sending a set of data buffers
+    int nBufs;     //number of data buffers
+    int bufSize;     //size of the buffer
+    int type;        //type of transfer scenario
+
+    
     struct addrinfo hints;
     struct addrinfo *result, *rp;
+
     int clientSD = -1;
 
 
     /*
      *  Argument validation
      */
-    if (argc != 2)
+    if (argc != 7)
     {
        cerr << "Usage: " << argv[0] << "serverName" << endl;
        return -1;
@@ -42,13 +51,19 @@ int main(int argc, char *argv[])
 	 * This addrinfo structure has internet address which can be used to create a socket too
      */
     serverName = argv[1];
+    strcpy(port, argv[2]);
+    repetitions = atoi(argv[3]);
+    nBufs = atoi(argv[4]);
+    bufSize = atoi(argv[5]);
+    type = atoi(argv[6]);
     
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;					/* Allow IPv4 or IPv6*/
     hints.ai_socktype = SOCK_STREAM;					/* TCP */
     hints.ai_flags = 0;							/* Optional Options*/
     hints.ai_protocol = 0;						/* Allow any protocol*/
-    int rc = getaddrinfo(serverName, serverPort, &hints, &result);
+
+    int rc = getaddrinfo(serverName, port, &hints, &result);
     if (rc != 0)
     {
        cerr << "ERROR: " << gai_strerror(rc) << endl;
@@ -64,7 +79,7 @@ int main(int argc, char *argv[])
         if (clientSD == -1)
 		{
             continue;
-        }
+      }
 		/*
 		* A socket has been successfully created
 		*/
@@ -95,18 +110,73 @@ int main(int argc, char *argv[])
     /*
      *  Write and read data over network
      */
-    databuf = new char[BUFFSIZE];
-    for (int i = 0; i < BUFFSIZE; i++)
-    {
-        databuf[i] = 'z';
-    }
- 
-    int bytesWritten = write(clientSD, databuf, BUFFSIZE);
-    cout << "Bytes Written: " << bytesWritten << endl;
+    char databuf[nBufs][bufSize];
 
-    int bytesRead = read(clientSD, databuf, BUFFSIZE);
-    cout << "Bytes Read: " << bytesRead << endl;
-    cout << databuf[13] << endl;
+    //send number of repetitions
+    int bytesWritten = write(clientSD, &repetitions, sizeof(int));
+    if(bytesWritten < 0)
+    {
+      cerr << "Write Failed" << endl;
+      close(clientSD);
+      return -1;
+    }
+
+    auto start = high_resolution_clock::now();  
+    for (int i = 0; i < repetitions; i++) {
+
+        switch(type) 
+        {
+         case 1: 
+
+            for(int j = 0; j < nBufs; j++){
+               int bytesWritten = write(clientSD, databuf[j], bufSize);
+                if(bytesWritten < 0)
+               {
+                  cerr << "Write Failed" << endl;
+                  close(clientSD);
+                  return -1;
+               }
+            }
+            break;
+
+         case 2:{
+            struct iovec vector[nBufs];
+            for (int j = 0; j < nBufs; j++) 
+            {
+               vector[j].iov_base = databuf[j];
+               vector[j].iov_len = bufSize;
+            }
+
+            int bytesWritten = writev(clientSD, vector, nBufs);
+            if(bytesWritten < 0)
+            {
+                  cerr << "Write Failed" << endl;
+                  close(clientSD);
+                  return -1;
+            }
+            break;
+         }
+
+         case 3: 
+            int bytesWritten = write(clientSD, databuf, nBufs * bufSize);
+            if(bytesWritten < 0)
+            {
+               cerr << "Write Failed" << endl;
+               close(clientSD);
+               return -1;
+            }
+            break;
+      }
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start).count();
+    int numReads;
+    read(clientSD, &numReads, sizeof(int));
+    double throughput = (nBufs * bufSize * 8.0) / (duration / 1000000.0); // Calculate throughput in Gbps
+
+    cout << "Test (" << type << "): time = " << duration << " usec, #reads = " 
+         << numReads << ", throughput " << throughput << " Gbps" << endl;
+    
 
     close(clientSD);
     return 0;
--- server.cpp
+++ server.cpp
@@ -19,23 +19,82 @@ using namespace std;
 
 const int BUFFSIZE = 1500;
 const int NUM_CONNECTIONS = 5;
+int TOTAL_READS = 0;
+
+struct ThreadArgs {
+   int clientSD;
+};
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *serviceConnect(void *arg){
+   
+   ThreadArgs *threadArgs = (ThreadArgs*) arg;
+   int clientSD = threadArgs->clientSD;
+   delete threadArgs;
+
+   char databuff[BUFFSIZE];
+   bzero(databuff, BUFFSIZE);
+
+   //read iterations from client
+   int iterations;
+   int bytesRead = read(clientSD, &iterations, sizeof(iterations));
+   if (bytesRead < 0) {
+        cerr << "Read failed" << endl;
+        close(clientSD);
+        pthread_exit(NULL);
+   }
+
+   int numReadCalls = 0;
+   for(int i = 0; i < iterations; i++){
+      int totalBytes = 0;
+      while(totalBytes < BUFFSIZE){
+         
+         int bytes = read(clientSD, databuff + totalBytes, BUFFSIZE - totalBytes);
+         TOTAL_READS++;
+         if (bytes < 0)
+         {
+            cerr << "Read Failed" << endl;
+            close(clientSD);
+            pthread_exit(NULL);
+         }
+         totalBytes += bytes;
+         numReadCalls++;
+      }
+   }
+
+    //Send the number of read calls made back
+    int bytesWritten = write(clientSD, &numReadCalls, sizeof(numReadCalls));
+    if (bytesWritten < 0){
+
+      cerr << "Write Failed" << endl;
+      close(clientSD);
+      pthread_exit(NULL);
+    }
+
+    close(clientSD);
+    pthread_exit(NULL);
+}
 
 int main(int argc, char *argv[])
 {
     int serverPort;
-    char *serverName;
-    char databuf[BUFFSIZE];
-    bzero(databuf, BUFFSIZE);
 
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " port" << endl;
+        return -1;
+    }
+
+    serverPort = atoi(argv[1]);
     /* 
      * Build address
      */
-    int port = 12345;
     sockaddr_in acceptSocketAddress;
     bzero((char *)&acceptSocketAddress, sizeof(acceptSocketAddress));
     acceptSocketAddress.sin_family = AF_INET;
     acceptSocketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    acceptSocketAddress.sin_port = htons(port);
+    acceptSocketAddress.sin_port = htons(serverPort);
 
     /*
      *  Open socket and bind
@@ -49,32 +108,55 @@ int main(int argc, char *argv[])
     if (rc < 0)
     {
         cerr << "Bind Failed" << endl;
+        return -1;
     }
 
     /*
      *  listen and accept
      */
     listen(serverSD, NUM_CONNECTIONS);       //setting number of pending connections
-    sockaddr_in newSockAddr;
-    socklen_t newSockAddrSize = sizeof(newSockAddr);
-    int newSD = accept(serverSD, (sockaddr *) &newSockAddr, &newSockAddrSize);
-    cout << "Accepted Socket #: " << newSD <<endl;
+    cout << "Waiting for connection..." << endl;
+    pthread_t threads[NUM_CONNECTIONS];
+    int threadCount = 0;
+    
+    while(true){
 
-    /*
-     *  read from the socket
-     */
-    int bytesRead = read(newSD, databuf, BUFFSIZE);
-    cout << "Bytes Read: " << bytesRead << endl;
-    cout << databuf[0] << endl;
+      sockaddr_in newSockAddr;
+      socklen_t newSockAddrSize = sizeof(newSockAddr);
+      int newSD = accept(serverSD, (sockaddr *) &newSockAddr, &newSockAddrSize);
+      cout << "Accepted Socket #: " << newSD <<endl;
 
-    /*
-     *  write to socker
-    */
-    databuf[13] = 'R';
-    int bytesWritten = write(newSD, databuf, BUFFSIZE);
-    cout << "Bytes Written: " << bytesWritten << endl;
+      ThreadArgs *threadArgs = new ThreadArgs;
+      threadArgs->clientSD = newSD;
 
-    close(newSD);
+      //critical section
+      pthread_mutex_lock(&mutex);
+      
+      int tid = pthread_create(&threads[threadCount], NULL, serviceConnect, (void*) threadArgs);
+      if(tid != 0){
+         cerr << "Error creating thread: " << tid << endl;
+         close(newSD);
+         delete threadArgs;
+         continue;
+      }
+
+      threadCount++;
+      pthread_mutex_unlock(&mutex);
+      //end critical section
+
+      //join threads
+      if(threadCount >= NUM_CONNECTIONS){
+         for(int i = 0; i < NUM_CONNECTIONS; i++){
+            tid = pthread_join(threads[i], NULL);
+            if(tid != 0){
+               cerr << "Error joining threads" << endl;
+            }
+         }
+         threadCount = 0;
+      }
+    }
+
+    pthread_mutex_destroy(&mutex);
     close(serverSD);
 
     return 0;
